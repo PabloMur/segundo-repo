2302920110b8457b760e5ea19c200761b6675d14
pablo <pablomurillo.sp@gmial.com>
2020-12-09 17:15:56 -0300

    primer comit

---
+++ app.js
@@ -0,0 +1,16 @@
+// let ROOT = document.getElementById("root")
+
+// function agregar (elemento, text) {
+//     return elemento.innerHTML = `
+//         <p>${text}</p>
+    
+//     `
+// }
+
+const botonContacto = document.querySelector(".contacto");
+botonContacto.addEventListener("click", hola);
+
+function hola(){
+    alert("hola perreke");
+};
+
